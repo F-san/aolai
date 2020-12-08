8e2ebb4fad065d047f59a14a3e08c284337c23f5
F-san <635660264@qq.com>
2020-12-08 10:42:35 +0800

    首页布局完成

--- js/index.js
+++ js/index.js
@@ -18,5 +18,42 @@ var swiper = new Swiper('.swiper-container', {
 
 /* 侧边栏 */
 $(function () {
-    
+    $(".sider-left").children("li").bind({
+        mouseover: function () {
+            $(this).children(".sider-i-text").stop().animate({
+                left: -50
+            })
+            $(this).css({
+                'border-top-left-radius': 0,
+                'border-bottom-left-radius': 0,
+                "border-left": "none"
+            });
+        },
+        mouseout: function () {
+            $(this).children(".sider-i-text").stop().animate({
+                left: 0
+            })
+            $(this).css({
+                'border-top-left-radius': 10,
+                'border-bottom-left-radius': 10,
+                "border-left": "1px solid #f0f0f0"
+            });
+        },
+        click: function () {
+            $(".sider").stop().animate({
+                right: -5
+            })
+        }
+    })
+    $(".sider-del").click(function () {
+        $(".sider").stop().animate({
+            right: -340
+        })
+    })
+    $(".backtop").off("click");
+    $(".backtop").click(function () {
+        $("html,body").animate({
+            scrollTop: 0
+        })
+    })
 })
