a19b8f98d951ba789ddb1b3c2e0ad5356b4ea981
F-san <635660264@qq.com>
2020-12-10 11:02:02 +0800

    添加搜索框

--- js/index.js
+++ js/index.js
@@ -15,9 +15,9 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
-/* 侧边栏 */
+
 $(function () {
-    
+    /* 侧边栏 */
     $(".sider-left").children("li").bind({
         mouseover: function () {
             $(this).children(".sider-i-text").stop().animate({
@@ -56,6 +56,7 @@ $(function () {
             scrollTop: 0
         })
     })
+
     updata();
     // 购物车数据更新
     function updata() {
@@ -66,7 +67,6 @@ $(function () {
                 "id": 45112
             },
             success: function (res) {
-                console.log(res.data);
                 let data = res.data;
                 navNemb(data);
             }
@@ -81,5 +81,29 @@ $(function () {
         $(".cart-number").html(num);
     }
 })
-
+/* 搜索框 */
+let oInput = document.querySelector(".search-input");
+let oUl = document.querySelector(".search-ul");
+oInput.oninput = () => {
+    let oScript = document.createElement("script");
+    oScript.src =
+        `https://suggest.taobao.com/sug?code=utf-8&q=${oInput.value}&extras=1&area=c2c&bucketid=atb_search&pid=mm_130402922_1108650181_109789550220&unid=&clk1=&_=1605832432438&callback=searchNav`;
+    document.body.appendChild(oScript);
+    document.body.removeChild(oScript);
+    oUl.style.display = "block";
+}
+oInput.onblur = () => {
+    oInput.value = "";
+    oUl.style.display = "none";
+}
+function searchNav(data) {
+    let result = data.result;
+    let str = "";
+    result.forEach(item => {
+        str += `
+        <li><a href="#">${item[0]}</a></li> 
+        `;
+    });
+    oUl.innerHTML = str;
+}
 
